341162a1cdfe20aa18e3adf050ddc26925a7d79d
Christian <christian.loewens@mailbox.org>
2023-05-18 15:42:49 +0200

    removed bootstrap js

--- download_resources.py
+++ download_resources.py
@@ -7,12 +7,8 @@ data = pd.read_csv(CSV_FILE, delimiter=";", decimal=",", header=9)
 data[(data.Gebietsart == "Bund") & (data.Gruppenart == "Partei") & (data.Stimme == 2) ][["Gruppenname", "Prozent"]].dropna().to_json("data.json", orient="values")
 
 
-# Get Bootstrap Bundle and Style Sheet
-
-JS_FILE = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js'
+# Get Bootstrap Style Sheet
 CSS_FILE = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-r = requests.get(JS_FILE, allow_redirects=True)
-open('bootstrap.bundle.min.js', 'wb').write(r.content)
 
 r = requests.get(CSS_FILE, allow_redirects=True)
 open('bootstrap.min.css', 'wb').write(r.content)
